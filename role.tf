51eed955d1c7494ffb646e10d4218efb7013b0f5
shabana <shabana.bellary@cloudreach.com>
2022-07-20 18:35:24 +0530

    role mofified and updated

--- role.tf
+++ role.tf
@@ -21,7 +21,7 @@ data "aws_iam_policy_document" "github_assume_role_policy" {
       }
 
     condition {
-        test = "StringEquals"
+        test = "StringLike"
         variable = "token.actions.githubusercontent.com:sub"
         values =  ["repo:shabana-github/*:*"]
       }
